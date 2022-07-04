0818183e6a23409a368f552375393d85e32acd48
OzirnaRita <ritkapuh1@gmail.com>
2022-07-05 00:54:41 +0300

    Add C/F transform

--- src/main.js
+++ src/main.js
@@ -11,9 +11,9 @@ let description = document.querySelector(".description");
 let days = [
     "Sunday",
     "Monday",
-    "Tuethday",
+    "Tuesday",
     "Wednesday",
-    "Thuersday",
+    "Thursday",
     "Friday",
     "Saturday"
 ];
@@ -33,58 +33,81 @@ let monthes = [
     "Decembre"
 ];
 let month = monthes[date.getMonth()];
-
 let actualDate = document.querySelector(".actualDate");
-actualDate.innerHTML = `${month} ${date.getDate()}`;
 let actualDay = document.querySelector(".actualDay");
-actualDay.innerHTML = `${day},`;
 let actualTime = document.querySelector(".actualTime");
 let minutes = date.getMinutes();
-if (minutes.toString().length === 1) {
+let hours = date.getHours();
+let form = document.querySelector("form");
+let icon = document.querySelector('#icon');
+let tempC = document.querySelector('.tempC');
+let tempF = document.querySelector('.tempF');
+let temp =  15;
+let feels = 14;
+
+
+if (minutes < 10) {
     minutes = "0" + minutes;
-    console.log(minutes);
 }
-actualTime.innerHTML = `${date.getHours()}:${minutes}`;
+if (hours < 10) {
+    hours = "0" + hours;
+}
+
+actualDate.innerHTML = `${month} ${date.getDate()}`;
+actualDay.innerHTML = `${day},`;
+actualTime.innerHTML = `${hours}:${minutes}`;
 
 function changeCity(event) {
     event.preventDefault();
-    let correctCity = city.value.charAt(0).toUpperCase() + city.value.slice(1);
-    actualCity.innerHTML = correctCity;
-    let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${correctCity}&units=metric`;
+    actualCity.innerHTML = city.value;
+    let apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric`;
     axios.get(`${apiWeather}&appid=${apiKey}`).then(currentTemp);
     form.reset();
 }
-
-let form = document.querySelector("form");
-form.addEventListener("submit", changeCity);
 
 function showGeo() {
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 function showPosition(position) {
-    console.log(position);
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-
     let apiWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
     axios.get(`${apiWeather}&appid=${apiKey}`).then(currentTemp);
 }
 
 function currentTemp(response) {
-    console.log(response);
-    let temp = Math.round(response.data.main.temp);
-    let feels = Math.round(response.data.main.feels_like);
+    temp = Math.round(response.data.main.temp);
+    feels = Math.round(response.data.main.feels_like);
     let wind = Math.round(response.data.wind.speed);
     let hum = Math.round(response.data.main.humidity);
     let des = response.data.weather[0].main;
 
-    actualDegree.innerHTML = `${temp}&deg;`;
+    actualDegree.innerHTML = `${temp}`;
     actualCity.innerHTML = response.data.name;
     feelTemperature.innerHTML = `${feels}`;
     windSpeed.innerHTML = `${wind}`;
     humidity.innerHTML = `${hum}`;
     description.innerHTML = `${des}`;
+    icon.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    icon.setAttribute('alt', `http://openweathermap.org/img/wn/${response.data.weather[0].desciption}`)
+}
+function displayFahrenheit(){
+    let tempFahrenheit = Math.round((temp *9)/5 + 32);
+    let feelsTempFahrenheit = Math.round((feels * 9) / 5 + 32);
+    actualDegree.innerHTML = `${tempFahrenheit}`;
+    feelTemperature.innerHTML = `${feelsTempFahrenheit}`;
+    tempC.classList.remove('active');
+    tempF.classList.add('active');
+}
+function displayCelsium() {
+    actualDegree.innerHTML = `${temp}`;
+    feelTemperature.innerHTML = `${feels}`;
+    tempF.classList.remove('active');
+    tempC.classList.add('active');
 }
 
 loc.addEventListener("click", showGeo);
+form.addEventListener("submit", changeCity);
+tempF.addEventListener('click', displayFahrenheit);
+tempC.addEventListener('click', displayCelsium)
